3777eb5555a52f5a9f5f82765537d2be2cb12754
Przemyslaw Szelenberger <przemek.szelenberger@performgroup.com>
2015-02-11 12:10:47 +0100

    Add Todo model.

---
+++ js/models/todo.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+  defaults: {
+    title: '',
+    completed: false
+  },
+
+  toggle: function(){
+    this.save({
+      completed: !this.get('completed')
+    });
+  }
+});
