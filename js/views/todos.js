d2a6bad232d3fd3b4ccd262308974ffd7adbeff1
Przemyslaw Szelenberger <przemek.szelenberger@performgroup.com>
2015-02-18 14:53:05 +0100

    Completing & deleting todos.

--- js/views/todos.js
+++ js/views/todos.js
@@ -15,7 +15,9 @@ app.TodoView = Backbone.View.extend({
 
   // The DOM events specific to an item.
   events: {
+    'click .toggle': 'toggleCompleted',
     'dblclick label': 'edit',
+    'click .destroy': 'clear',
     'keypress .edit': 'updateOnEnter',
     'blur .edit': 'close'
   },
@@ -25,14 +27,39 @@ app.TodoView = Backbone.View.extend({
   // app, we set a direct reference on the model for convenience.
   initialize: function(){
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   // Rerenders the titles of the todo item.
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
+
+    this.$el.toggleClass('completed', this.model.get('completed'));
+    this.toggleVisible();
+
     this.$input = this.$('.edit');
 
     return this;
+  },
+
+  // Toggles visibility of item
+  toggleVisible: function(){
+    this.$el.toggleClass('hidden', this.isHidden());
+  },
+
+  // Determines if item should be hidden
+  isHidden: function(){
+    var isCompleted = this.model.get('computed');
+
+    return (
+      (!isCompleted && app.TodoFilter === 'completed') ||
+      (isCompleted && app.TodoFilter === 'active')
+    );
+  },
+
+  toggleCompleted: function(){
+    this.model.toggle();
   },
 
   // Switch this view into `"editing"` mode, displaying the input field.
@@ -59,5 +86,10 @@ app.TodoView = Backbone.View.extend({
     if (e.which === ENTER_KEY) {
       this.close();
     }
+  },
+
+  // Remove the item, destroy the model from *localStorage* and delete its view.
+  clear: function(){
+    this.model.destroy();
   }
 });
